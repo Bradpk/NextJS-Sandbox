ff9e0921ba2878125b8904dc770eaa678a7a6dea
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-10-06 15:09:13 -0400

    g

--- nextjs-sandbox/src/app/rockpaperscissors.js
+++ nextjs-sandbox/src/app/rockpaperscissors.js
@@ -1,5 +1,67 @@
-function RockPaperScissors () {
+'use client';
+import {useState} from 'react';
 
+function RockPaperScissors () {
+    const getUserChoice = userInput => {
+        userInput = userInput.toLowerCase();
+        if(userInput == 'rock' || userInput == 'paper' || userInput == 'scissors'){
+          return userInput 
+        } else {
+          console.log('Not a valid option, must choose rock, paper or scissors')
+        }
+        }
+        
+        const getComputerChoice = () => {
+        let randomNumber = Math.floor(Math.random() * 3);
+        switch(randomNumber) {
+          case 0:
+          return 'rock';
+          break;
+          case 1:
+          return 'paper';
+          break;
+          case 2:
+          return 'scissors'
+          break;
+        }
+        }
+        
+        const determineWinner = (userChoice, computerChoice) => {
+          if (userChoice === computerChoice){
+            return 'Draw'
+          } 
+          if (userChoice === 'paper'){
+            if(computerChoice === 'rock'){
+              return 'You Won!'
+            } else {
+              return 'Computer Won!'
+            }
+          }
+          if (userChoice === 'scissors'){
+            if (computerChoice ==='rock'){
+              return 'Computer Won!'
+            } else {
+              return 'You Won!'
+            }
+          }
+          if(userChoice === 'rock'){
+            if(computerChoice === 'paper'){
+              return 'Computer Won!'
+            } else {
+              return 'You Won!'
+            }
+          }
+        }
+        
+        const playGame = () => {
+          let userChoice = getUserChoice('rock')
+          let computerChoice = getComputerChoice()
+          console.log(userChoice)
+          console.log(computerChoice)
+          console.log(determineWinner(userChoice, computerChoice))
+        }
+        
+        playGame()
 }
 
 export default RockPaperScissors
