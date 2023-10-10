50fc1b8fddf9cbe2c347b9f0e68b29c3e2cb78dd
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-10-10 12:54:59 -0400

    g

---
+++ nextjs-sandbox/src/app/sleepcalculator.js
@@ -0,0 +1,66 @@
+const getSleepHours = (day) => {
+    switch (day) {
+      case'Monday':
+      return 7
+      break;
+      case 'Tuesday':
+      return 8
+      break;
+      case'Wednesday':
+      return 9
+      break;
+      case 'Thursday':
+      return 7
+      break;
+      case 'Friday':
+      return 8 
+      break; 
+      case 'Saturday':
+      return 9 
+      break; 
+      case 'Sunday':
+      return 7
+      break; 
+      default:
+      return 'Not Valid'
+      break;
+    }
+  }
+  
+  const getActualSleepHours = () => 
+    getSleepHours('Monday') +
+    getSleepHours('Tuesday') +
+    getSleepHours('Wednesday') +
+    getSleepHours('Thursday') +
+    getSleepHours('Friday') +
+    getSleepHours('Saturday') +
+    getSleepHours('Sunday') 
+  
+  
+  const getIdealSleepHours = () => {
+    let idealHours = 8
+    return idealHours * 7
+  }
+  
+  
+  const calculateSleepDebt = () => {
+    let actualSleepHours = getActualSleepHours()
+    let idealSleepHours = getIdealSleepHours()
+  
+    if(actualSleepHours === idealSleepHours){
+      console.log('Perfect amount of sleep')
+      
+    } else if (actualSleepHours > idealSleepHours) {
+      console.log('You overslept by ' + (actualSleepHours - idealSleepHours) + ' hours')
+    } else if(actualSleepHours < idealSleepHours) {
+      console.log('You need ' + (idealSleepHours - actualSleepHours) + ' more hours of sleep')
+    }
+  }
+  
+  console.log(calculateSleepDebt())
+
+
+
+
+
+  //convert this to react
