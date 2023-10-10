386410d5655fb188bacfddef1aab67bdecad5e9b
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-10-10 13:59:10 -0400

    g

--- nextjs-sandbox/src/app/sleepcalculator.js
+++ nextjs-sandbox/src/app/sleepcalculator.js
@@ -2,44 +2,11 @@
 import {useState} from "react";
 
 function SleepCalculator () {
-const getSleepHours = (day) => {
-    switch (day) {
-      case'Monday':
-      return 7
-      break;
-      case 'Tuesday':
-      return 8
-      break;
-      case'Wednesday':
-      return 9
-      break;
-      case 'Thursday':
-      return 7
-      break;
-      case 'Friday':
-      return 8 
-      break; 
-      case 'Saturday':
-      return 9 
-      break; 
-      case 'Sunday':
-      return 7
-      break; 
-      default:
-      return 'Not Valid'
-      break;
+    const [getSleepHours, setGetSleepHours] = useState('');
+
+    const handleChange = (event) => {
+        setGetSleepHours(event.target.value)
     }
-  }
-  
-  const getActualSleepHours = () => 
-    getSleepHours('Monday') +
-    getSleepHours('Tuesday') +
-    getSleepHours('Wednesday') +
-    getSleepHours('Thursday') +
-    getSleepHours('Friday') +
-    getSleepHours('Saturday') +
-    getSleepHours('Sunday') 
-  
   
   const getIdealSleepHours = () => {
     let idealHours = 8
@@ -48,16 +15,15 @@ const getSleepHours = (day) => {
   
   
   const calculateSleepDebt = () => {
-    let actualSleepHours = getActualSleepHours()
+    let actualSleepHours = getSleepHours
     let idealSleepHours = getIdealSleepHours()
   
-    if(actualSleepHours === idealSleepHours){
-      console.log('Perfect amount of sleep')
-      
+    if(actualSleepHours == idealSleepHours){
+      return('Perfect amount of sleep')
     } else if (actualSleepHours > idealSleepHours) {
-      console.log('You overslept by ' + (actualSleepHours - idealSleepHours) + ' hours')
+      return('You overslept by ' + (actualSleepHours - idealSleepHours) + ' hours')
     } else if(actualSleepHours < idealSleepHours) {
-      console.log('You need ' + (idealSleepHours - actualSleepHours) + ' more hours of sleep')
+      return('You need ' + (idealSleepHours - actualSleepHours) + ' more hours of sleep')
     }
   }
   
@@ -68,10 +34,10 @@ const getSleepHours = (day) => {
 return (
     <>
     <p>Sleep Debt Calculator</p>
-    <p>Week Goal: 56 hours of sleep</p>
-    <input></input>
-    <button></button>
-    <p></p>
+    <p>Enter total hours of sleep per week</p>
+    <input onChange={handleChange} value={getSleepHours}></input>
+    <button onClick={calculateSleepDebt}>Calculate</button>
+    <p>{calculateSleepDebt()}</p>
     </>
 )}
 
